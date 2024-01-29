b3ddae2c588f7cb4360e7071b2b99d1ad5884747
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-01-30 00:36:53 +0530

    Add files via upload
    #Day 1
    Q1.Simple Java Program.
    Q2.Print Integer in java.

---
+++ Integer.java
@@ -0,0 +1,10 @@
+public class Integer {
+    public static void main(String[] arguments) { 
+        //declaring a variable 
+            int c; 
+        /* Using for loop to repeat */ 
+            for (c = 1; c <= 10; c++) { 
+            System.out.println(c); 
+        } 
+        }
+}
