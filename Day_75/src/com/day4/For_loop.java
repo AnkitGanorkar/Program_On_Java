2036e06f1a9ad3d1809316706a95a885c7c6db4f
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-02 21:38:08 +0530

    For Loops

---
+++ Day_75/src/com/day4/For_loop.java
@@ -0,0 +1,10 @@
+package com.day4;
+
+public class For_loop {
+	public static void main(String[] args) {
+		System.out.println(":Printing numbers from 1 to 10 using a for Loops:");
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + " ");
+		}
+	}
+}
