460c23c842098af6df733bd6c9326bc6b7a6c3e0
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-04 20:13:51 +0530

    Advance Patterns

---
+++ Day_75/src/com/day6/Advance_Pattern_3.java
@@ -0,0 +1,20 @@
+package com.day6;
+
+public class Advance_Pattern_3 {
+	public static void main(String args[]) {
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+			// spaces
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+
+			// numbers
+			for (int j = 1; j <= i; j++) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+	}
+}
