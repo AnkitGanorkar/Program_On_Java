b2ae85097983c3823ea94057402925ecea0971e9
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-05 23:16:41 +0530

    Pattern

---
+++ Day_75/src/com/day5/Pattern_5.java
@@ -0,0 +1,12 @@
+package com.day5;
+
+public class Pattern_5 {
+	public static void main(String[] args) {
+		for (int i = 5; i >= 1; i--) {
+			for (int j = 5; j >= 1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+}
