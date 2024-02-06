265188a0962aea2f61fa13e2dde105b1584bcac1
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-06 22:56:09 +0530

    Pattern Program

---
+++ Day_75/src/com/day8/Pattern_11.java
@@ -0,0 +1,12 @@
+package com.day5;
+
+public class Pattern_11 {
+	public static void main(String[] args) {
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=3;j++) {
+				System.out.printf(i+" "+j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
