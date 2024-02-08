50f51f25439bca9128d39330e4a490296de130ba
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-08 20:04:07 +0530

    Sorting_Array

---
+++ Day_75/src/com/day10/Sorting_Array.java
@@ -0,0 +1,11 @@
+package com.day10;
+
+import java.util.Arrays;
+
+public class Sorting_Array {
+	 public static void main(String[] args) {
+	        int[] numbers = {30, 10, 50, 20, 40};
+	        Arrays.sort(numbers);
+	        System.out.println("Sorted array: " + Arrays.toString(numbers));
+	    }
+}
