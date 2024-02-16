e6fd197123828776b40df6f40e00bb19950cb9b3
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-16 22:28:33 +0530

    Wave_Pattern2

---
+++ Day_75/src/com/day18/Wave_Pattern2.java
@@ -0,0 +1,22 @@
+package com.day18;
+
+public class Wave_Pattern2 {
+	public static void main(String[] args) {
+		int wave_height = 8;
+		int wL = 8;
+		int wH = wave_height - 1;
+		int x = wH;
+		for (int i = 0; i <= wH; i++) {
+			for (int j = 0; j <= wH * wL; j++) {
+				if (j % (wH * 2) == x || j % (wH * 2) == wH + i) {
+					System.out.print("|");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			x--;
+			System.out.println();
+
+		}
+	}
+}
