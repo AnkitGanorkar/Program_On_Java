4c17cc71324f93d909ef5c070e24c9977b22b755
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-16 21:26:27 +0530

    Wave_Pattern3

---
+++ Day_75/src/com/day17/Wave_Pattern3.java
@@ -0,0 +1,26 @@
+package com.day17;
+
+public class Wave_Pattern3 {
+	public static void main(String[] args) {
+		int wave_height = 5;
+		int wave_length = 4;
+
+		int i;
+		int j;
+		int k;
+
+		for (i = 1; i <= wave_height; i++) {
+			for (j = 1; j <= wave_length; j++) {
+				for (k = 1; k <= wave_height; k++) {
+					if (i == k || i + k == wave_height + 1) {
+						System.out.print("*");
+					} else {
+						System.out.print(" ");
+					}
+				}
+			}
+			System.out.print("\n");
+		}
+	}
+
+}
