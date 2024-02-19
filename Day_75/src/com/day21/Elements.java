3ef7018e3f785a23bd0bf209ae67b393aca38019
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-19 23:39:19 +0530

    Elements

---
+++ Day_75/src/com/day21/Elements.java
@@ -0,0 +1,17 @@
+package com.day21;
+
+import java.util.LinkedList;
+
+public class Elements {
+	public static void main(String[] args) {
+
+		// create a linkedlist
+		LinkedList<String> languages = new LinkedList<>();
+
+		// Add elements to LinkedList
+		languages.add("Java");
+		languages.add("Python");
+		languages.add("JavaScript");
+		System.out.println("LinkedList: " + languages);
+	}
+}
