52a80915bda51c9d0f54ccc67bdd25d8fda11cb9
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-19 23:40:10 +0530

    Specified_Position

---
+++ Day_75/src/com/day21/Specified_Position.java
@@ -0,0 +1,20 @@
+package com.day21;
+
+import java.util.LinkedList;
+
+public class Specified_Position {
+	public static void main(String[] args) {
+
+		// create a linkedlist
+		LinkedList<String> languages = new LinkedList<>();
+
+		// add elements to LinkedList
+		languages.add("Swift");
+		languages.add("Python");
+		System.out.println("LinkedList: " + languages);
+
+		// add element at the specified position
+		languages.add(0, "Java");
+		System.out.println("Updated LinkedList: " + languages);
+	}
+}
