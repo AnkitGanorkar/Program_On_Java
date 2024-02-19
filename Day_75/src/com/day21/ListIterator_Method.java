5b3551daead9169a953e71e5065cbd75dab2c632
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-19 23:29:45 +0530

    ListIterator_Method.java

---
+++ Day_75/src/com/day21/ListIterator_Method.java
@@ -0,0 +1,32 @@
+package com.day21;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class ListIterator_Method {
+	 public static void main(String[] args) {
+	        LinkedList<String> animals= new LinkedList<>();
+
+	        // Add elements in LinkedList
+	        animals.add("Dog");
+	        animals.add("Horse");
+	        animals.add("Cat");
+
+	        // Create an object of ListIterator
+	        ListIterator<String> listIterate = animals.listIterator();
+	        System.out.print("LinkedList: ");
+
+	        while(listIterate.hasNext()) {
+	            System.out.print(listIterate.next());
+	            System.out.print(", ");
+	        }
+
+	        // Iterate backward
+	        System.out.print("\nReverse LinkedList: ");
+
+	        while(listIterate.hasPrevious()) {
+	            System.out.print(listIterate.previous());
+	            System.out.print(", ");
+	        }
+	    }
+}
