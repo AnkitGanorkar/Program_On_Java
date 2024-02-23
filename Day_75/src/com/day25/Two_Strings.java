b823ebd66c4b7c80023424e7952ad2339b3a7204
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-23 23:43:50 +0530

    Compare two strings

---
+++ Day_75/src/com/day25/Two_Strings.java
@@ -0,0 +1,15 @@
+package com.day25;
+
+public class Two_Strings {
+	//Compare two strings
+	public static void main(String[] args) {
+
+		String style = "Bold";
+		String style2 = "Bold";
+
+		if (style == style2)
+			System.out.println("Equal");
+		else
+			System.out.println("Not Equal");
+	}
+}
