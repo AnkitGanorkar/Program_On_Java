19ba7c6ff838b8e9854d982ff867ce04638dc54e
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-24 21:47:49 +0530

    Perimeter of Triangle
    Java Program to Find Perimeter of Triangle Using static value

---
+++ Day_75/src/com/day26/Perimeter_of_Triangle01.java
@@ -0,0 +1,15 @@
+package com.day26;
+
+public class Perimeter_of_Triangle01 {
+	// Java Program to Find Perimeter of Triangle Using static value
+	// Driver method
+	public static void main(String[] args) {
+		// length of 3 sides of triangle declared
+		double side1 = 2.0;
+		double side2 = 3.0;
+		double side3 = 5.0;
+		// finding the perimeter
+		double perimeter = (side1 + side2 + side3);
+		System.out.println("Perimeter of triangle = " + perimeter);
+	}
+}
