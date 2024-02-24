05badc3c856daac518e7596990a8c1e9c8ac6449
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-24 21:55:16 +0530

    Area and Perimeter
    Java Program to Find Area and Perimeter of Rectangle By using dynamic values.

---
+++ Day_75/src/com/day26/Area_and_Perimeter02.java
@@ -0,0 +1,25 @@
+package com.day26;
+
+import java.util.Scanner;
+
+public class Area_and_Perimeter02 {
+	// Java Program to Find Area and Perimeter of Rectangle By using dynamic values
+	public static void main(String[] args) {
+		// variables declared
+		int length, breadth, perimeter, area;
+		Scanner sc = new Scanner(System.in);
+		// taking length input from user
+		System.out.print("Enter length of rectangle : ");
+		length = sc.nextInt();
+		// taking breadth input from user
+		System.out.print("Enter breadth of rectangle : ");
+		breadth = sc.nextInt();
+		// finding perimeter
+		perimeter = 2 * (length + breadth);
+		System.out.println("Perimeter of rectangle : " + perimeter);
+		// finding area
+		area = length * breadth;
+		System.out.println("Area of rectangle : " + area);
+	}
+
+}
