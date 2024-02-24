f31a5150175fa0fb2ce19aa29aa977155c464f9b
AnkitRganorkar <nktganorkar@gmail.com>
2024-02-24 21:59:39 +0530

    update

--- Day_75/src/com/day26/Area_and_Perimeter02.java
+++ Day_75/src/com/day26/Area_and_Perimeter02.java
@@ -3,6 +3,7 @@ package com.day26;
 import java.util.Scanner;
 
 public class Area_and_Perimeter02 {
+	
 	// Java Program to Find Area and Perimeter of Rectangle By using dynamic values
 	public static void main(String[] args) {
 		// variables declared
