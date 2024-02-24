286e1365e2918a8b36ed07b7030706868dae7799
Ankit Rajesh Ganorkar <124537476+AnkitGanorkar@users.noreply.github.com>
2024-02-24 21:35:18 +0530

    Area of Triangle
    Java Program to Find Area of Triangle Using Static Value

---
+++ Day_75/src/com/day26/Area_of_Triangle.java
@@ -0,0 +1,14 @@
+package com.day26;
+
+public class Area_of_Triangle {
+	// Java Program to Find Area of Triangle Using Static Value
+	public static void main(String args[]) { // width of triangle
+		double width = 20.0;
+		// hight of triangle
+		double height = 100.5;
+		// finding area
+		double area = (width * height) / 2;
+		// printing the area
+		System.out.println("Area of Triangle: " + area);
+	}
+}
